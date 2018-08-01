6a9f43d582a193cdd8b0e24b64c15c3c7ad25be4
Madison <madisonsadison@gmail.com>
2018-08-01 10:53:56 -0600

    fixed a naming error

--- Number_Theory/GCD_LCM_by_Euclidean_algorithm.py
+++ Number_Theory/GCD_LCM_by_Euclidean_algorithm.py
@@ -2,6 +2,7 @@
 Function as taught in An Illustrated Theory of Numbers by Martin H. Weissman.
 Function definition and more on the Euclidean Algorithm can be found in Chapter
 1 on page 25.
+LCM algorithm demonstrated on page 39.
 """
 def get_GCD_euclidean_algorithm(a, b):
     while a % b != 0:
@@ -12,4 +13,13 @@ def get_GCD_euclidean_algorithm(a, b):
     return c
 
 
+def get_LCM_euclidean_algorithm(a, b):
+    # LCM = a x b / gcd(a, b) (for positive a and b)
+    # LCM(a, b) = LCM(|a|, |b|) thus we do not need to consider negative integers
+    gcd = get_GCD_euclidean_algorithm(a, b)
+    lcm = a * b / gcd
+    return lcm
+
+
 print(get_GCD_euclidean_algorithm(2059, 1711))
+print(get_LCM_euclidean_algorithm(2059, 1711))
